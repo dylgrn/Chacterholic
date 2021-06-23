282f315ccccf01462b0b8341cc876635389dbab2
shanemcf <shanemcfadden95@yahoo.com>
2021-06-22 20:42:44 -0500

    added landing page and login page with sign up

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,7 +1,21 @@
 const router = require('express').Router();
 
 router.get('/', (req, res) => {
-    res.render('homepage');
+    res.render('homepage', {
+        id: 1,
+        post_url: 'https://handlebarsjs.com/guide/',
+        title: 'Handlebars Docs',
+        created_at: new Date(),
+        vote_count: 10,
+        comments: [{}, {}],
+        user: {
+            username: 'test_user'
+        }
+    });
+});
+
+router.get('/login', (req, res) => {
+    res.render('login');
 });
 
 module.exports = router;
