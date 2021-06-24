9876c9177c6fe900fd2999530ec88714dc85a587
Gandalf <dylangreen034@gmail.com>
2021-06-23 20:35:23 -0500

    items

---
+++ models/items.js
@@ -0,0 +1,48 @@
+// Set Handlebars as the default template engine
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+// Data
+// =============================================================
+const items = [
+  {
+    itemType: 'Long Sword',
+    attack: 20,
+    diff: 15
+  },
+  {
+    itemType: 'Long Bow',
+    attack: 18,
+    deff: 8
+  },
+  {
+    itemType: 'Axe',
+    attack: 28,
+    deff: 18
+  },
+  {
+    itemType: 'Knives',
+    attack: 17,
+    deff: 12
+  },
+  {
+    itemType: 'Staff',
+    attack: 15,
+    deff: 10
+  },
+  {
+    itemType: 'potion'
+  }
+];
+
+// Routes
+// =============================================================
+
+app.get('/all-items', (req, res) => {
+  // Handlebars requires an object to be sent to the index.handlebars file.
+  const data = {
+    items: []
+  };
+
+  res.render('items', data);
+});
