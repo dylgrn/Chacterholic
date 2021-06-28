8a43fd13a9ceedd7fc8a22971caa5a7d3f683cc7
Justin Hoffman <justinhoffman193@gmail.com>
2021-06-28 12:29:15 -0500

    added character sheet and character route

---
+++ controllers/home-routes.js
@@ -0,0 +1,22 @@
+const router = require('express').Router();
+
+router.get('/', (req, res) => {
+    res.render('homepage', {
+        id: 1,
+        post_url: 'https://handlebarsjs.com/guide/',
+        title: 'Handlebars Docs',
+        created_at: new Date(),
+        user: {
+            username: 'test_user'
+        }
+    });
+});
+
+router.get('/login', (req, res) => {
+    res.render('login');
+});
+
+router.get('/character', (req, res) => {
+    res.render('character');
+  });
+module.exports = router;
